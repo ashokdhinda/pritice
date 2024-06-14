3fb442ec92b253c7c58a1df285c378ed5c923125
ashokdhinda <ashokkudhinda143@gmail.com>
2024-06-14 07:23:43 +0530

    Factory function complet

---
+++ Dsa/Reverse_array/index.js
@@ -0,0 +1,40 @@
+// function reversestring(num) {
+//     return num = num.split("").reverse().join("")
+// }
+// let string = reversestring("Ashok kumar dhinda")
+// console.log(string);
+
+
+// // function number(string) {
+//     let bag = ""
+//     for (let i = string.length- 1; i >= 0; i--){
+//         bag = bag + string[i];
+//     }
+//     console.log(bag);
+    
+// }
+// number("Asish")
+
+//check palindrom
+
+// function palindrom(string) {
+//     return string == string.split("").reverse().join("")
+
+// }
+// let newa = palindrom("racar")
+
+// console.log(newa);
+
+// function palindrom(string) {
+//     let bag = ''
+//     for (let i = string.length - 1; i >= 0; i--){
+//        bag = bag+string[i]
+      
+//     }
+//     return bag == string
+  
+// }
+
+// let numm= palindrom("ashok")
+// console.log(numm);
+
