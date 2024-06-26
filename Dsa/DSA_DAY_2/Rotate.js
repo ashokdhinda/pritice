db235203b5c851955070237de418d365b1963929
ashokdhinda <ashokkudhinda143@gmail.com>
2024-06-26 10:52:50 +0530

    problem solving in arr rotate

--- Dsa/DSA_DAY_2/Rotate.js
+++ Dsa/DSA_DAY_2/Rotate.js
@@ -1,7 +1,8 @@
-
+// Rotate the array;
+// Given N element array ,Rotate array from last to first k times .<google,meta,amazon>; K=4
 
 function rotate(arr, k) {
-    
+    //first reverse all element
     let sp = 0;
     let ep = arr.length - 1;
     while (sp < ep) {
@@ -11,6 +12,7 @@ function rotate(arr, k) {
         sp++;
         ep--
     }
+    //here reverse the first k element
     let ksp = 0;
     let kep = k - 1;
     while (ksp<kep) {
@@ -21,8 +23,19 @@ function rotate(arr, k) {
         kep--
         
     }
+    //here reverse the k element to arr.length-1
+    let starp = k;
+    let endp = arr.length - 1;
+    while (starp < endp) {
+        let temp = arr[starp]
+        arr[starp] = arr[endp]
+        arr[endp] = temp;
+        starp++;
+        endp--;
+    }
     console.log(arr);
+
 }
 
-let newarr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let newarr = [1, 2, 3, 4, 5, 6, 7, 8,9]
 rotate(newarr,4)
